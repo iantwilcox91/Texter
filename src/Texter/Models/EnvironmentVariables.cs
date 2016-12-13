f2f21de2badfa2947d79ab2e3a5249ed497bd64a
Jonathan Buchner <websites@jonathanbuchner.com>
2016-12-12 17:31:49 -0800

    add project

---
+++ src/Texter/Models/EnvironmentVariables.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Texter.Models
+{
+    public class EnvironmentVariables
+    {
+        public static string AccountSid = "AC40c2ea7ea65d4a7d9a61983bc54df189";
+        public static string AuthToken = "67804bcce2bce939cc8e1a5542f9ef92";
+    }
+}
---
+++ src/Texter/Models/Message.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using RestSharp.Authenticators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Texter.Models
+{
+    public class Message
+    {
+        public string To { get; set; }
+        public string From { get; set; }
+        public string Body { get; set; }
+        public string Status { get; set; }
+        public static List<Message> GetMessages()
+        {
+            var client = new RestClient("https://api.twilio.com/2010-04-01");
+            var request = new RestRequest("Accounts/" + EnvironmentVariables.AccountSid + "/Messages.json", Method.GET);
+            client.Authenticator = new HttpBasicAuthenticator(EnvironmentVariables.AccountSid, EnvironmentVariables.AuthToken);
+            //2
+            var response = new RestResponse();
+            //3a
+            Task.Run(async () =>
+            {
+                response = await GetResponseContentAsync(client, request) as RestResponse;
+            }).Wait();
+            //4
+            JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(response.Content);
+            var messageList = JsonConvert.DeserializeObject<List<Message>>(jsonResponse["messages"].ToString());
+            return messageList;
+            
+        }
+           
+        public void Send()
+        {
+            var client = new RestClient("https://api.twilio.com/2010-04-01");
+            var request = new RestRequest("Accounts/" + EnvironmentVariables.AccountSid + "/Messages", Method.POST);
+            request.AddParameter("To", To);
+            request.AddParameter("From", From);
+            request.AddParameter("Body", Body);
+            client.Authenticator = new HttpBasicAuthenticator(EnvironmentVariables.AccountSid, EnvironmentVariables.AuthToken);
+            client.ExecuteAsync(request, response => {
+                Console.WriteLine(response.Content);
+            });
+        }
+
+        public static Task<IRestResponse> GetResponseContentAsync(RestClient theClient, RestRequest theRequest)
+        {
+            var tcs = new TaskCompletionSource<IRestResponse>();
+            theClient.ExecuteAsync(theRequest, response => {
+                tcs.SetResult(response);
+            });
+            return tcs.Task;
+        }
+    }
+}
